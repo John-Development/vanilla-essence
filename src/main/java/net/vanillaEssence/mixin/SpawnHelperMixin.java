61074366d02a5aad09c69be55de94c879b327a21
John-Development <38006800+John-Development@users.noreply.github.com>
2020-12-14 13:18:01 +0100

    First commit

---
+++ src/main/java/net/vanillaEssence/mixin/SpawnHelperMixin.java
@@ -0,0 +1,81 @@
+package net.vanillaEssence.mixin;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
+import net.minecraft.entity.SpawnGroup;
+import net.minecraft.entity.decoration.EndCrystalEntity;
+import net.minecraft.server.world.ServerWorld;
+import net.minecraft.world.SpawnHelper;
+import net.minecraft.world.biome.SpawnSettings;
+import net.minecraft.world.gen.StructureAccessor;
+import net.minecraft.world.gen.chunk.ChunkGenerator;
+import net.vanillaEssence.util.PropertiesCache;
+
+@Mixin(SpawnHelper.class)
+public abstract class SpawnHelperMixin {
+
+  @Inject(
+    method = "canSpawn",
+    at = @At("HEAD"),
+    cancellable = true
+  )
+  private static void handleCustomSpawnRestriction(
+    ServerWorld world,
+    SpawnGroup group,
+    StructureAccessor pAccessor,
+    ChunkGenerator chunkGenerator,
+    SpawnSettings.SpawnEntry spawnEntry,
+    BlockPos.Mutable blockPos,
+    double squaredDistance,
+    CallbackInfoReturnable<Boolean> cir
+  ) {
+    PropertiesCache cache = PropertiesCache.getInstance();
+    
+    if (group != null
+      && group.compareTo(SpawnGroup.MONSTER) == 0
+      && Boolean.parseBoolean(cache.getProperty("crystal-enabled"))
+    ) {
+      double monsterX, monsterY, monsterZ = 0;
+      monsterX = blockPos.getX();
+      monsterY = blockPos.getY();
+      monsterZ = blockPos.getZ();
+
+      int radius = Integer.parseInt(cache.getProperty("crystal-radius"));
+      int lowerLimitDistance = Integer.parseInt(cache.getProperty("crystal-lower-limit-distance"));
+
+      Box box = new Box(
+        monsterX + radius,
+        monsterY + lowerLimitDistance,
+        monsterZ + radius,
+        monsterX - radius,
+        monsterY - 2 * radius - lowerLimitDistance,
+        monsterZ - radius
+      );
+
+      List<EndCrystalEntity> crystals = world.getEntitiesByClass(EndCrystalEntity.class, box, new Predicate<EndCrystalEntity>() {
+        @Override
+          public boolean test(EndCrystalEntity crystal) {
+            return (!cache.getProperty("crystal-name").trim().isEmpty())
+              ? cache.getProperty("crystal-name").equals(crystal.getName().asString())
+              : true;
+          }
+        }
+      );
+
+      if (crystals != null && !crystals.isEmpty()) {
+        cir.setReturnValue(false);
+        return;
+      }
+    }
+    return;
+  }
+  
+}
---
+++ src/main/java/net/vanillaEssence/vanillaEssence.java
@@ -0,0 +1,49 @@
+package net.vanillaEssence;
+
+import java.io.File;
+import java.io.IOException;
+
+import net.fabricmc.api.ModInitializer;
+import net.vanillaEssence.util.PropertiesCache;
+
+public class vanillaEssence implements ModInitializer {
+
+  final public static String DEF_CRYSTAL_RAD = "32";
+  final public static String DEF_CRYSTAL_LIM_DISTANCE = "1";
+  final public static String DEF_CRYSTAL_NAME = "";
+  final public static String DEF_SCAFF_LIMIT = "";
+  final public static String CONFIG_FILE = "essence.properties";
+  @Override
+  public void onInitialize() {
+    try {
+      File configFile = new File(CONFIG_FILE);
+      // If config file does not exists creates a new one with the default values
+      configFile.createNewFile();
+
+      PropertiesCache cache = PropertiesCache.getInstance();
+      if (cache.getProperty("crystal-enabled") == null) {
+        cache.setProperty("crystal-enabled", "true");
+      }
+      if (cache.getProperty("crystal-radius") == null) {
+        cache.setProperty("crystal-radius", DEF_CRYSTAL_RAD);
+      }
+      if (cache.getProperty("crystal-lower-limit-distance") == null){
+        cache.setProperty("crystal-lower-limit-distance", DEF_CRYSTAL_LIM_DISTANCE);
+      }
+      if (cache.getProperty("crystal-name") == null){
+        cache.setProperty("crystal-name", DEF_CRYSTAL_NAME);
+      }
+      if (cache.getProperty("scaff-enabled") == null){
+        cache.setProperty("scaff-enabled", "true");
+      }
+      if (cache.getProperty("scaff-limit") == null){
+        cache.setProperty("scaff-limit", DEF_SCAFF_LIMIT);
+      }
+      
+      //Write to the file
+      PropertiesCache.getInstance().flush();
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+}
