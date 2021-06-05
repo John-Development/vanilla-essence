4b556dd96dc9e69f9bdf9dba77e2e1da6369f7a5
John-Development <38006800+John-Development@users.noreply.github.com>
2021-06-05 17:12:36 +0200

    Updated magnetic lure code to 1.17

--- src/main/java/net/vanillaEssence/mixin/entity/ItemEntityMixin.java
+++ src/main/java/net/vanillaEssence/mixin/entity/ItemEntityMixin.java
@@ -14,7 +14,6 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
 import net.minecraft.nbt.NbtList;
 import net.minecraft.nbt.NbtElement;
-import net.minecraft.sound.SoundEvents;
 import net.minecraft.tag.FluidTags;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Vec3d;
@@ -32,7 +31,6 @@ public abstract class ItemEntityMixin extends Entity {
   public int orbAge;
   public int pickupDelay;
   private PlayerEntity target;
-  private int lastTargetUpdateTick;
 
   private void applyWaterMovement() {
     Vec3d vec3d = this.getVelocity();
@@ -48,39 +46,33 @@ public abstract class ItemEntityMixin extends Entity {
   ) {
     PropertiesCache cache = PropertiesCache.getInstance();
     if (Boolean.parseBoolean(cache.getProperty("magnetic-lure-enabled"))) {
-      if (this.pickupDelay > 0) {
-        --this.pickupDelay;
-      }
-
+      super.tick();
       this.prevX = this.getX();
       this.prevY = this.getY();
       this.prevZ = this.getZ();
-      
+
       if (this.isSubmergedIn(FluidTags.WATER)) {
-          this.applyWaterMovement();
+         this.applyWaterMovement();
       } else if (!this.hasNoGravity()) {
-          this.setVelocity(this.getVelocity().add(0.0D, -0.03D, 0.0D));
+         this.setVelocity(this.getVelocity().add(0.0D, -0.03D, 0.0D));
       }
 
       if (this.world.getFluidState(this.getBlockPos()).isIn(FluidTags.LAVA)) {
-          this.setVelocity((double)((this.random.nextFloat() - this.random.nextFloat()) * 0.2F), 0.20000000298023224D, (double)((this.random.nextFloat() - this.random.nextFloat()) * 0.2F));
-          this.playSound(SoundEvents.ENTITY_GENERIC_BURN, 0.4F, 2.0F + this.random.nextFloat() * 0.4F);
+         this.setVelocity((double)((this.random.nextFloat() - this.random.nextFloat()) * 0.2F), 0.20000000298023224D, (double)((this.random.nextFloat() - this.random.nextFloat()) * 0.2F));
       }
 
       if (!this.world.isSpaceEmpty(this.getBoundingBox())) {
-          this.pushOutOfBlocks(this.getX(), (this.getBoundingBox().minY + this.getBoundingBox().maxY) / 2.0D, this.getZ());
+         this.pushOutOfBlocks(this.getX(), (this.getBoundingBox().minY + this.getBoundingBox().maxY) / 2.0D, this.getZ());
       }
 
-      if (this.lastTargetUpdateTick < this.renderTicks - 20 + this.getId() % 100) {
-          if (this.target == null || this.target.squaredDistanceTo(this) > 64.0D) {
-            this.target = this.world.getClosestPlayer(this, 8.0D);
-          }
-
-          this.lastTargetUpdateTick = this.renderTicks;
+      if (this.age % 20 == 1) {
+        if (this.target == null || this.target.squaredDistanceTo(this) > 64.0D) {
+          this.target = this.world.getClosestPlayer(this, 8.0D);
+        }
       }
 
-      if (this.target != null && this.target.isSpectator()) {
-          this.target = null;
+      if (this.target != null && (this.target.isSpectator() || this.target.isDead())) {
+         this.target = null;
       }
 
       if (this.target != null) {
@@ -90,41 +82,90 @@ public abstract class ItemEntityMixin extends Entity {
           if (!itemStack.getItem().equals(Items.FISHING_ROD)) {
             for (NbtElement tag : listTag) {
               
-              // Example: {lvl:1s,id:"minecraft:protection"};
+              // Example: {id:"minecraft:lure",lvl:1s};
               Boolean hasLure = tag.toString().contains("id:\"minecraft:lure\"");
+
+              System.out.println(tag.toString());
       
               if (hasLure) {
-                int lvl = Integer.parseInt(Character.toString(tag.toString().charAt(5)));
+                int lvl = Integer.parseInt(Character.toString(tag.toString().split(",")[1].charAt(4)));
 
                 // attracts every item as an xp orb
                 Vec3d vec3d = new Vec3d(this.target.getX() - this.getX(), this.target.getY() + (double)this.target.getStandingEyeHeight() / 2.0D - this.getY(), this.target.getZ() - this.getZ());
-                double e = vec3d.lengthSquared();
-                if (e < 64.0D) {
-                  double f = 1.0D - Math.sqrt(e) / 8.0D;
-                  this.setVelocity(this.getVelocity().add(vec3d.normalize().multiply(lvl * f * f * 0.1D)));
+                double d = vec3d.lengthSquared();
+                if (d < 64.0D) {
+                  double e = 1.0D - Math.sqrt(d) / 8.0D;
+                  this.setVelocity(this.getVelocity().add(vec3d.normalize().multiply(lvl * e * e * 0.1D)));
                 }
-
                 this.move(MovementType.SELF, this.getVelocity());
-                float g = 0.98F;
+                float f = 0.98F;
                 if (this.onGround) {
-                    g = this.world.getBlockState(new BlockPos(this.getX(), this.getY() - 1.0D, this.getZ())).getBlock().getSlipperiness() * 0.98F;
+                    f = this.world.getBlockState(new BlockPos(this.getX(), this.getY() - 1.0D, this.getZ())).getBlock().getSlipperiness() * 0.98F;
                 }
 
-                this.setVelocity(this.getVelocity().multiply((double)g, 0.98D, (double)g));
+                this.setVelocity(this.getVelocity().multiply((double)f, 0.98D, (double)f));
                 if (this.onGround) {
                     this.setVelocity(this.getVelocity().multiply(1.0D, -0.9D, 1.0D));
                 }
 
-                ++this.renderTicks;
                 ++this.orbAge;
                 if (this.orbAge >= 6000) {
-                    this.remove(RemovalReason.UNLOADED_WITH_PLAYER);
+                    this.discard();
                 }
               }
             }
           }
         }
       }
+
+      // if (this.target != null) {
+      //   Iterable<ItemStack> equipped = target.getItemsEquipped();
+      //   for (ItemStack itemStack : equipped) {
+      //     NbtList listTag = itemStack.getEnchantments();
+      //     if (!itemStack.getItem().equals(Items.FISHING_ROD)) {
+      //       for (NbtElement tag : listTag) {
+              
+      //         // Example: {lvl:1s,id:"minecraft:protection"};
+      //         Boolean hasLure = tag.toString().contains("id:\"minecraft:lure\"");
+      
+      //         if (hasLure) {
+      //           int lvl = Integer.parseInt(Character.toString(tag.toString().charAt(5)));
+
+      //           // attracts every item as an xp orb
+      //           Vec3d vec3d = new Vec3d(this.target.getX() - this.getX(), this.target.getY() + (double)this.target.getStandingEyeHeight() / 2.0D - this.getY(), this.target.getZ() - this.getZ());
+      //           double e = vec3d.lengthSquared();
+      //           if (e < 64.0D) {
+      //             double f = 1.0D - Math.sqrt(e) / 8.0D;
+      //             this.setVelocity(this.getVelocity().add(vec3d.normalize().multiply(lvl * f * f * 0.1D)));
+      //           }
+
+      //           this.move(MovementType.SELF, this.getVelocity());
+      //           float g = 0.98F;
+      //           if (this.onGround) {
+      //               g = this.world.getBlockState(new BlockPos(this.getX(), this.getY() - 1.0D, this.getZ())).getBlock().getSlipperiness() * 0.98F;
+      //           }
+
+      //           this.setVelocity(this.getVelocity().multiply((double)g, 0.98D, (double)g));
+      //           if (this.onGround) {
+      //               this.setVelocity(this.getVelocity().multiply(1.0D, -0.9D, 1.0D));
+      //           }
+
+      //           ++this.renderTicks;
+      //           ++this.orbAge;
+      //           if (this.orbAge >= 6000) {
+      //               this.remove(RemovalReason.UNLOADED_WITH_PLAYER);
+      //           }
+      //         }
+      //       }
+      //     }
+      //   }
+      // }
+    }
+  }
+
+  private void onEverySecond() {
+    if (this.target == null || this.target.squaredDistanceTo(this) > 64.0D) {
+       this.target = this.world.getClosestPlayer(this, 8.0D);
     }
   }
 }
