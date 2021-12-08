af45eaaba4da30dda315d6780531c711efea85ae
John-Development <38006800+John-Development@users.noreply.github.com>
2021-12-08 19:31:13 +0100

    Fix redstone power bug

--- src/main/java/net/vanillaEssence/mixin/block/JukeboxBlockMixin.java
+++ src/main/java/net/vanillaEssence/mixin/block/JukeboxBlockMixin.java
@@ -2,7 +2,6 @@ package net.vanillaEssence.mixin.block;
 
 import net.minecraft.block.*;
 import net.minecraft.entity.player.PlayerEntity;
-import net.minecraft.state.StateManager;
 import net.minecraft.state.property.BooleanProperty;
 import net.minecraft.state.property.Properties;
 import net.minecraft.util.ActionResult;
@@ -15,8 +14,6 @@ import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
 
 @Mixin(JukeboxBlock.class)
 public abstract class JukeboxBlockMixin extends BlockWithEntity {
@@ -25,20 +22,7 @@ public abstract class JukeboxBlockMixin extends BlockWithEntity {
   @Shadow
   public static final BooleanProperty HAS_RECORD;
 
-  private static final BooleanProperty TRIGGERED;
-
   @Shadow protected abstract void removeRecord(World world, BlockPos pos);
-
-  @ModifyArg(
-    method = "<init>",
-    at = @At(value = "INVOKE",
-      target = "Lnet/minecraft/block/JukeboxBlock;setDefaultState(Lnet/minecraft/block/BlockState;)V"
-    ),
-    index = 0
-  )
-  private BlockState getBlockState(BlockState state) {
-    return state.with(TRIGGERED, false);
-  }
 
   public JukeboxBlockMixin(AbstractBlock.Settings settings) {
     super(settings);
@@ -47,7 +31,12 @@ public abstract class JukeboxBlockMixin extends BlockWithEntity {
   @Override
   public void neighborUpdate(BlockState state, World world, BlockPos pos, Block block, BlockPos fromPos, boolean notify) {
     if (PropertiesCache.getInstance().getBoolProperty("redstoned-jukeboxes-enabled")) {
-      this.updateEnabled(world, pos, state);
+      boolean powered = world.isReceivingRedstonePower(pos);
+      if (powered) {
+        this.removeRecord(world, pos);
+        state = state.with(HAS_RECORD, false);
+        world.setBlockState(pos, state, 2);
+      }
     }
   }
 
@@ -65,23 +54,7 @@ public abstract class JukeboxBlockMixin extends BlockWithEntity {
     }
   }
 
-  private void updateEnabled(World world, BlockPos pos, BlockState state) {
-    boolean powered = PropertiesCache.getInstance().getBoolProperty("redstoned-jukeboxes-enabled") && world.isReceivingRedstonePower(pos);
-    if (powered == state.get(TRIGGERED)) {
-      world.setBlockState(pos, state.with(TRIGGERED, !powered), 4);
-      if (powered) {
-        this.removeRecord(world, pos);
-      }
-    }
-  }
-
-  @Override
-  public void appendProperties(StateManager.Builder<Block, BlockState> builder) {
-    builder.add(TRIGGERED, HAS_RECORD);
-  }
-
   static {
     HAS_RECORD = Properties.HAS_RECORD;
-    TRIGGERED = Properties.TRIGGERED;
   }
 }
