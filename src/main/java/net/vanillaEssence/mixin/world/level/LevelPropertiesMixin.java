31f88a25f1778cde1cfc8f40b49a78ac96bb3d9a
John-Development <38006800+John-Development@users.noreply.github.com>
2023-01-14 23:39:58 +0100

    Update to 1.19.3 [2]

--- src/main/java/net/vanillaEssence/mixin/world/level/LevelPropertiesMixin.java
+++ src/main/java/net/vanillaEssence/mixin/world/level/LevelPropertiesMixin.java
@@ -20,7 +20,26 @@ import java.util.UUID;
 
 @Mixin(LevelProperties.class)
 public class LevelPropertiesMixin {
-  @Inject(method = "<init>(Lcom/mojang/datafixers/DataFixer;ILnet/minecraft/nbt/NbtCompound;ZIIIFJJIIIZIZZZLnet/minecraft/world/border/WorldBorder$Properties;IILjava/util/UUID;Ljava/util/Set;Lnet/minecraft/world/timer/Timer;Lnet/minecraft/nbt/NbtCompound;Lnet/minecraft/nbt/NbtCompound;Lnet/minecraft/world/level/LevelInfo;Lnet/minecraft/world/gen/GeneratorOptions;Lcom/mojang/serialization/Lifecycle;)V", at = @At("TAIL"))
+  @Inject(
+    method = "<init>(" +
+      "Lcom/mojang/datafixers/DataFixer;" +
+      "I" +
+      "Lnet/minecraft/nbt/NbtCompound;" +
+      "ZIIIFJJIIIZIZZZ" +
+      "Lnet/minecraft/world/border/WorldBorder$Properties;" +
+      "II" +
+      "Ljava/util/UUID;" +
+      "Ljava/util/Set;" +
+      "Lnet/minecraft/world/timer/Timer;" +
+      "Lnet/minecraft/nbt/NbtCompound;" +
+      "Lnet/minecraft/nbt/NbtCompound;" +
+      "Lnet/minecraft/world/level/LevelInfo;" +
+      "Lnet/minecraft/world/gen/GeneratorOptions;" +
+      "Lnet/minecraft/world/level/LevelProperties$SpecialProperty;" +
+      "Lcom/mojang/serialization/Lifecycle;" +
+    ")V",
+    at = @At("TAIL")
+  )
   void initializer(DataFixer dataFixer, int dataVersion, NbtCompound playerData, boolean modded, int spawnX, int spawnY, int spawnZ, float spawnAngle, long time, long timeOfDay, int version, int clearWeatherTime, int rainTime, boolean raining, int thunderTime, boolean thundering, boolean initialized, boolean difficultyLocked, WorldBorder.Properties worldBorder, int wanderingTraderSpawnDelay, int wanderingTraderSpawnChance, UUID wanderingTraderId, Set<String> serverBrands, Timer<MinecraftServer> scheduledEvents, NbtCompound customBossEvents, NbtCompound dragonFight, LevelInfo levelInfo, GeneratorOptions generatorOptions, Lifecycle lifecycle, CallbackInfo ci) {
     PropertiesCache.setLevelName(levelInfo.getLevelName());
   }
